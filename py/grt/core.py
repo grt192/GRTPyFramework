4a5ee600070a05fec559bf22d6f83c1ce48a2532
Calvin Huang <clhuang@eneron.us>
2013-10-18 22:34:27 -0700

    Added accel, Constants (we still want those) Forgot to delete moved files

--- py/grt/core.py
+++ py/grt/core.py
@@ -11,6 +11,12 @@ class Sensor:
             return self.state[name]
         return 0
 
+    def get(self, name):
+        '''
+        Returns the datum associated with some name.
+        '''
+        return self.state[name]
+
     def __dir__(self):
         return sorted(set(dir(type(self) + self.state.keys())))
 
@@ -31,10 +37,11 @@ class Sensor:
     def update_state(self, state_id, datum):
         '''
         Updates the state of this sensor.
+
         Updates state state_id with data datum.
-        Also notifies listeners of state change.
+        Also notifies listeners of state change (on change, not add).
         '''
-        if state_id not in self.state or self.state[state_id] != datum:
+        if state_id in self.state and self.state[state_id] != datum:
             self.state[state_id] = datum
             for l in self.listeners:
                 l(self, state_id, datum)
@@ -56,13 +63,80 @@ class SensorPoller:
         self.sensors = sensors
         self.polltime = polltime
 
-    def start(self):
-        self.running = True
-        self.t = threading.Thread(target=self.run)
+    def add_sensor(sensor):
+        '''
+        Adds a sensor to poll.
+        '''
+        sensors.append(sensor)
 
-    def run(self):
+    def remove_sensor(sensor):
+        '''
+        Removes a sensor.
+        '''
+        sensors.remove(sensor)
+
+    def halt(self):
+        '''
+        Stops this thread from polling.
+        '''
+        self.running = False
+
+    def start(self):
+        '''
+        Starts polling.
+        '''
+        self.running = True
+        self.t = threading.Thread(target=self._run)
+
+    def _run(self):
         while self.running:
             for sensor in self.sensors:
                 sensor.poll()
             time.sleep(self.polltime)
+
+class Constants(Sensor):
+    '''
+    Class for reading, and keeping track of, constants in a file.
+
+    File is read line by line, in [key],[number] format.
+    Lines starting with '/' are treated as add'l constants files,
+    and are read recursively.
+    Blank lines, and lines starting with '#' are ignored.
+
+    Behaves more or less like a sensor.
+    '''
+
+    def __init__(file_loc='/c/constants.txt'):
+        self.file_loc = file_loc
+
+    def poll(self):
+        '''
+        Reloads file data.
+        '''
+        self.load_file(self.file_loc)
+
+    def load_file(file_loc):
+        '''
+        Loads file data from a file.
+        If other files are listed inside this file,
+        recursively loads them.
+
+        (Beware of "include loops" that will cause a crash)
+        '''
+        f = open(file_loc)
+        for line in f:
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+            if line.startswith('/'):  # is a file, load it!
+                self.load_file(line)
+                continue
+            # continue with getting data
+            key, _, value = line.partition(',')
+            try:
+                self.update_state(key, float(value))
+            except ValueError:
+                print("Malformed constants file " + file_loc +
+                      " with key " + key + " and value " + value)
+        f.close()
 
---
+++ py/grt/sensors/accelerometer.py
@@ -0,0 +1,22 @@
+from wpilib import ADXL345_I2C
+from grt.core import Sensor
+
+class Accelerometer(Sensor):
+    '''
+    Sensor wrapper for the ADXL345 accelerometer, connected via I2C.
+
+    Has double attributes for acceleration among the 3 axes: x/y/z_accel.
+    '''
+
+    def __init__(self, module=1,
+                 fmt=ADXL345_I2C.DataFormat_Range.k8G):
+        '''
+        Initializes the accelerometer on some module.
+        '''
+        super().__init__()
+        self.a = ADXL345_I2C(module, fmt)
+
+    def poll(self):
+        self.update_state('x_accel', self.a.GetAcceleration(ADXL345_I2C.Axes.kX))
+        self.update_state('y_accel', self.a.GetAcceleration(ADXL345_I2C.Axes.kY))
+        self.update_state('z_accel', self.a.GetAcceleration(ADXL345_I2C.Axes.kZ))
