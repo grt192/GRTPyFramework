342f1fef64ee7fb2a155e4b891d0122ec636c33e
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2013-10-19 22:36:09 -0700

    Actually adding config file

---
+++ py/config.py
@@ -0,0 +1,19 @@
+__author__ = "Sidd Karamcheti"
+
+import wpilib
+"""
+Config File for Robot 
+
+#TODO: Setup for Constants File
+"""
+
+# Joysticks
+lstick = wpilib.Joystick(1)
+rstick = wpilib.Joystick(2)
+
+
+#Solenoids (PINS TENTATIVE)
+solenoid = wpilib.Solenoid(7, 1)
+
+#Motors (PINS TENTATIVE)
+#motor = wpilib.CANJaguar(8)
