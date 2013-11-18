63c9de9885b2683418746dfc9a08b80fff9bdff8
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2013-11-18 15:39:01 -0800

    Trying super sketch shooter code

--- py/config.py
+++ py/config.py
@@ -14,13 +14,36 @@ from grt.mechanism.drivecontroller import ArcadeDriveController
 
 # Joysticks
 lstick = Attack3Joystick(1)
+rstick = Attack3Joystick(2)
 
-sp = SensorPoller((lstick, ))
+#Super Sketch Omega 1
+shooter_pivot_motor = wpilib.Talon(8)
+
+flywheel_1 = wpilib.Talon(9)
+flywheel_2 = wpilib.Talon(10)
+
+def flywheel_listener(source, id, datum):
+	if id == 'button3' and rstick.button3:
+		flywheel_1.Set(1)
+		flywheel_2.Set(1)
+	elif id == 'button3' and not rstick.button3:
+		flywheel_1.Set(0)
+		flywheel_2.Set(0)
+
+
+def shooter_pivot_listener(source, id, datum):
+    if id == 'y_axis':
+        shooter_pivot_motor.Set(datum * 0.25)
+
+for r in (flywheel_listener, shooter_pivot_listener):
+    rstick.add_listener(r)
+
+sp = SensorPoller((lstick, rstick, ))
 
 #Solenoids (PINS TENTATIVE)
 #solenoid = wpilib.Solenoid(7, 1)
 
-#Motors (PINS TENTATIVE)
+#Motors 
 lfm = wpilib.Talon(3)
 lrm = wpilib.Talon(4)
 rfm = wpilib.Talon(1)
