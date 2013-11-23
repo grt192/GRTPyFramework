78ade47c953c2fc9845276ced1091b1f1fd6a220
Calvin Huang <clhuang@eneron.us>
2013-11-22 20:34:01 -0800

    Revert "Adding working Omega 1 code"
    This reverts commit 9884360526778cb853af530592468856defa856e.

--- py/config.py
+++ py/config.py
@@ -27,22 +27,11 @@ lfm = wpilib.Talon(3)
 lrm = wpilib.Talon(4)
 rfm = wpilib.Talon(1)
 rrm = wpilib.Talon(2)
-
-shooter_pivot_motor = wpilib.Victor(8)
+shooter_pivot_motor = wpilib.Talon(8)
 flywheel_1 = wpilib.Talon(9)
-flywheel_2 = wpilib.Talon(10)
+flywheel_2 = wpilib.Talon(10
 
-belts = wpilib.Victor(7)
-ep_raiser = wpilib.Victor(6)
-ep_roller = wpilib.Victor(5)
-
-luna = wpilib.Solenoid(8)
-climber = wpilib.Solenoid(7)
-
-compressor = wpilib.Compressor(1, 1)
-compressor.Start()
-
-mechs = Mechanisms(rstick, flywheel_1, flywheel_2, shooter_pivot_motor, belts, ep_roller, luna, climber)
+mechs = Mechanisms(rstick, flywheel_1, flywheel_2, shooter_pivot_motor)
 dt = DriveTrain(lfm, rfm, lrm, rrm)
 dt.set_scale_factors(1, -1, 1, -1)
 
