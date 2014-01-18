79641eef130feba14728888225318990be88d9cf
ajnam12 <ajnamavari12@gmail.com>
2014-01-17 17:36:37 -0800

    Create autocontroller.py

---
+++ py/grt/mechanism/autocontroller.py
@@ -0,0 +1,7 @@
+#Run method that determines what the robot does during autonomous
+__author__ = 'Armin'
+from .. import macro.drive_macro
+def run():
+    dm = drive_macro.Macrodrive
+    #set set point
+    
