8431fa2fdf635272284a3490d9b2db71cb521fdb
Armin Namavari <ajnamavari12@gmail.com>
2014-01-18 12:46:47 -0800

    Fixed logic errors

--- py/grt/mechanism/mechcontroller.py
+++ py/grt/mechanism/mechcontroller.py
@@ -15,20 +15,29 @@ class AttackMechController:
                 self.intake.start_ep()
             elif state_id is 'button3':
                 self.intake.reverse()
-        elif sensor in self.l_joystick and state_id in ('button4', 'trigger'):
+        elif sensor in self.l_joystick:
             #Shooter Control
             if not datum:
                 self.shooter.winch_stop()
-            elif state_id is 'button4':
-                self.shooter.winch_wind(1)
+            if state_id is 'button4':
+                if datum:
+                    self.shooter.winch_wind(1)
+                else:
+                    self.shooter.winch_stop()
             elif state_id is 'trigger':
-                self.shooter.latch()
-        elif sensor in self.l_joystick and state_id in ('button8', 'button9', 'button5'):
+                if datum:
+                    self.shooter.extend()
+                else:
+                    self.shooter.latch()
+        elif sensor in self.l_joystick:
             #Defense Control
             if not datum:
                 self.defense.winch_stop()
-            elif state_id is 'button8':
-                self.defense.winch_wind(1)
+            if state_id is 'button8':
+                if datum:
+                    self.defense.winch_wind(1)
+                else:
+                    self.defense.winch_stop()
             elif state_id is 'button5':
                 self.defense.extend()
             elif state_id is 'button9':
