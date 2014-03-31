5dd2e8a382ce4ba691719ecfe8ed04d07f90c31d
Calvin Huang <clhuang@eneron.us>
2014-03-31 16:31:22 -0700

    Add comments, fix logic in conditional macro

--- py/grt/macro/conditional_macro.py
+++ py/grt/macro/conditional_macro.py
@@ -6,26 +6,30 @@ Executes one macro or another depending on the return value of a function.
 
 from grt.core import GRTMacro
 
+
 class ConditionalMacro(GRTMacro):
+    """
+    Executes one macro or another depending on a function call.
+    """
+    currmacro = None
+
     def __init__(self, function, macro1, macro2):
-        self.macro1 = macro1
+        """
+        If function() returns true upon initialization of this macro,
+        macro1 is run. Otherwise, macro2 is run.
+        """
+        self.currmacro = self.macro1 = macro1
         self.macro2 = macro2
-        self.macros = [self.macro1, self.macro2]
         self.function = function
 
     def initialize(self):
-        self.macro1.reset()
-        self.macro2.reset()
-        if self.function():
-            self.macro1.run()
-        else:
-            self.macro2.run()
+        self.currmacro = self.macro1 if self.function() else self.macro2
+        self.curmacro.reset()
+        self.currmacro.run()
 
     def perform(self):
-        if all((not m.running or m.daemon for m in self.macros)):
+        if not self.currmacro.running:
             self.kill()
 
     def die(self):
-        print('Concurrent Macro Finished')
-        for m in self.macros:
-            m.kill()
+        self.currmacro.kill()
