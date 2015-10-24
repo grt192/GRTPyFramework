bf547e66f48789f42dda99fe60ba9408348a614e
amallery <amallery@gmail.com>
2015-10-23 17:09:15 -0700

    Removed extra files, began configuring for haunted house

--- py/config.py
+++ py/config.py
@@ -11,174 +11,46 @@ from wpilib import Solenoid, Compressor, DriverStation, CANTalon, DigitalInput
 
 from grt.sensors.attack_joystick import Attack3Joystick
 from grt.sensors.xbox_joystick import XboxJoystick
-from grt.sensors.gyro import Gyro
 from grt.core import SensorPoller
-from grt.mechanism.drivetrain import DriveTrain
-from grt.mechanism.drivecontroller import ArcadeDriveController
-from grt.mechanism.motorset import Motorset
-from grt.sensors.ticker import Ticker
-from grt.sensors.encoder import Encoder
-from grt.sensors.talon import Talon
-from grt.mechanism.betamechs import FourBar, TwoMotorPickup
-from grt.mechanism.elevator import Elevator
+
 from grt.mechanism.mechcontroller import MechController
-#from grt.macro.align_macro import AlignMacro
-from grt.autonomous.basic_auto import BasicAuto
-from grt.autonomous.one_bin_stealer_auto import OneBinSteal
-from grt.autonomous.two_bin_stealer_auto import TwoBinSteal
-from grt.autonomous.backup_bin_stealer import BackupBinSteal
-from teleop_controller import TeleopController
+
 from record_controller import RecordMacro, PlaybackMacro
-from grt.mechanism.bin_steal_mech import BinStealMech
 from collections import OrderedDict
 
-#from grt.sensors.switch import Switch
-
-#import grt.networktables as networktables
-
-auto_enabled = True
-recording_enabled = False
+from grt.mechanism.hhmechanisms import Elmo, HeadPunch, Staircase, HeadlessMonkey
 
 
-#Pin/Port map
+elmo_motor = CANTalon(1)
+head_punch_motor = CANTalon(2)
+
+head_punch_actuator = Solenoid(0)
+staircase_actuator = Solenoid(1)
+headless_linear_act = Solenoid(2)
+headless_rotary_act = Solenoid(3)
 compressor = Compressor()
 compressor.start()
-solenoid1 = Solenoid(0)
-solenoid2 = Solenoid(1)
-solenoid3 = Solenoid(2)
-solenoid4 = Solenoid(3)
-bin_steal_mech = BinStealMech(solenoid1, solenoid2, solenoid3, solenoid4)
 
-#Talons
+elmo = Elmo(elmo_motor)
+head_punch = HeadPunch(head_punch_motor, head_punch_actuator)
+staircase = Staircase(staircase_actuator)
+headless_monkey = HeadlessMonkey(headless_linear_act, headless_rotary_act)
 
-#dt_right_alt = Talon(1)
-#dt_left_alt = Talon(2)
-
-dt_right = CANTalon(1)
-dt_r2 = CANTalon(2)
-dt_r3 = CANTalon(3)
-dt_r4 = CANTalon(4)
-
-dt_left = CANTalon(7)
-dt_l2 = CANTalon(8)
-dt_l3 = CANTalon(9)
-dt_l4 = CANTalon(10)
-
-dt_r2.changeControlMode(CANTalon.ControlMode.Follower)
-dt_r3.changeControlMode(CANTalon.ControlMode.Follower)
-dt_r4.changeControlMode(CANTalon.ControlMode.Follower)
-dt_l2.changeControlMode(CANTalon.ControlMode.Follower)
-dt_l3.changeControlMode(CANTalon.ControlMode.Follower)
-dt_l4.changeControlMode(CANTalon.ControlMode.Follower)
-dt_r2.set(1)
-dt_r3.set(1)
-dt_r4.set(1)
-dt_l2.set(7)
-dt_l3.set(7)
-dt_l4.set(7)
+elmo_arr = [elmo_motor]
+head_punch_arr = [head_punch_motor, head_punch_actuator]
+staircase_arr = [staircase_actuator]
+headless_arr = [headless_linear_act, headless_rotary_act]
+record_macro = RecordMacro(elmo_arr)
 
 
-
-gyro = Gyro(1)
-
-
-
-
-
-
-
-#if auto_enabled:
-left_encoder = Encoder(0, 1, distance_per_rev=(4 * math.pi), reverse=True)
-right_encoder = Encoder(4, 5, distance_per_rev=(4*math.pi))
-
-dt = DriveTrain(dt_left, dt_right, left_encoder=left_encoder, right_encoder=right_encoder)
-	#self.drive_macro = DriveMacro(dt, 10, 2)
-
-#Digital inputs
-#l_switch = DigitalInput(8)
-#r_switch = DigitalInput(9)
-
-
-elevator_motor = CANTalon(11)
-elevator_motor_2 = CANTalon(12)
-elevator_motor_2.changeControlMode(CANTalon.ControlMode.Follower)
-elevator_motor_2.set(11)
-
-
-
-elevator_distance_per_rev = 1.273 * math.pi
-#Changed to 120 for omega 2!
-elevator_cpr = 120
-elevator_encoder = Encoder(2, 3, distance_per_rev=elevator_distance_per_rev, cpr=elevator_cpr, reverse=False)
-top_switch = DigitalInput(13)
-bottom_limit_switch = DigitalInput(9)
-bottom_switch = DigitalInput(8)
-left_switch = DigitalInput(7)
-right_switch = DigitalInput(6)
-elevator = Elevator(elevator_motor, elevator_encoder, left_switch=left_switch, right_switch=right_switch, dt=dt, top_switch=top_switch, bottom_switch=bottom_switch, bottom_limit_switch=bottom_limit_switch)
-
-fourbar_motor = CANTalon(5)
-fourbar_distance_per_rev = 1
-fourbar_cpr = 120
-fourbar_encoder = Encoder(10, 11, distance_per_rev=fourbar_distance_per_rev, cpr=fourbar_cpr, reverse=False)
-fourbar = FourBar(fourbar_motor, fourbar_encoder)
-
-talon_arr_basic = [dt_right, dt_left, elevator.lift_macro]
-talon_arr = [dt_right, dt_left, elevator.lift_macro, fourbar_motor]
-#talon_arr_macro = [dt_right, dt_left, elevator.lift_macro, elevator_motor]
-record_macro = RecordMacro(talon_arr)
-
-basic_auto = BasicAuto(dt, elevator, talon_arr_basic)
-one_bin_steal = OneBinSteal(talon_arr)
-two_bin_steal = TwoBinSteal(talon_arr, bin_steal_mech, dt)
-backup_bin_steal = BackupBinSteal(talon_arr)
-playback_macro = backup_bin_steal.playback_macro
-
-
-
-sp = SensorPoller((gyro, left_encoder, right_encoder, elevator_encoder))
-
-#else:
-	#dt = DriveTrain(dt_left, dt_right, left_encoder=None, right_encoder=None)
-	#sp = SensorPoller((gyro,))
-
-
-
-
-
-
-#Analog Sensors
 
 # Controllers
 driver_stick = Attack3Joystick(0)
 xbox_controller = XboxJoystick(1)
-ac = ArcadeDriveController(dt, driver_stick, record_macro, playback_macro)
 hid_sp = SensorPoller((driver_stick, xbox_controller))  # human interface devices
 
-if recording_enabled:
-	from record_controller import RecordController, PlaybackController
-	from collections import OrderedDict
-	instructions = OrderedDict([("1, <class 'grt.sensors.talon.Talon'>", [0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 0.17142857142857137, 0.17142857142857137, 0.17142857142857137, 0.17142857142857137, 0.17142857142857137]), ("2, <class 'grt.sensors.talon.Talon'>", [0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, -0.8, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, 0.2571428571428571, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -0.6285714285714286, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, -1.1714285714285715, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 0.1428571428571429, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 1.2571428571428571, 0.17142857142857137, 0.17142857142857137, 0.17142857142857137, 0.17142857142857137, 0.17142857142857137])])
-	talon_arr = [dt_right, dt_left]
-	record_controller = RecordController([talon_arr[0], talon_arr[1]])
-	playback_controller = PlaybackController(instructions, talon_arr, revert_controller=drive_controller)
-	teleop_controller = TeleopController(sp, hid_sp, driver_stick, ac, record_controller, playback_controller)
-else:
-	teleop_controller = TeleopController(sp, hid_sp)
 
-
-
-#Mechs
-
-
-
-
-
-motor1 = CANTalon(14)
-motor2 = CANTalon(15)
-two_motor_pickup = TwoMotorPickup(motor1, motor2)
-
-mc = MechController(elevator, fourbar, two_motor_pickup, bin_steal_mech, driver_stick, xbox_controller)
+mc = MechController(elmo, head_punch, staircase, headless_monkey, record_macro, driver_stick, xbox_controller)
 
 ds = DriverStation.getInstance()
 
--- py/robot.py
+++ py/robot.py
@@ -6,8 +6,7 @@ import inspect
 
 import time
 
-auto = None
-auto_exists = True
+auto_exists = False
 
 
 class MyRobot(wpilib.SampleRobot):
@@ -15,62 +14,37 @@ class MyRobot(wpilib.SampleRobot):
         super().__init__()
         import config
         
-        self.sp = config.sp
         self.hid_sp = config.hid_sp
-        self.dt = config.dt
         self.ds = config.ds
-        self.teleop_controller = config.teleop_controller
-        self.elevator_encoder = config.elevator_encoder
-        self.driver_stick = config.driver_stick
-        global auto
-        global auto_exists
-        self.basic_auto = config.basic_auto
-        self.one_bin_steal = config.one_bin_steal
-        self.two_bin_steal = config.two_bin_steal
-        try:
-            auto = config.basic_auto
-        except AttributeError:
-            auto_exists = False
+        
 
 
     def disabled(self):
         global auto_exists
         if auto_exists:
-            self.basic_auto.stop_autonomous()
-            self.one_bin_steal.stop_autonomous()
-            self.two_bin_steal.stop_autonomous()
+            pass
         while self.isDisabled():
             tinit = time.time()
             self.sp.poll()
-            print(self.elevator_encoder.e.getDistance())
             self.safeSleep(tinit, .04)
     if auto_exists:
         def autonomous(self):
-            global auto
+        
             print("Autonomous started")
-            if self.driver_stick.j.getZ() > .5:
-                self.one_bin_steal.run_autonomous()
-            else:
-                self.two_bin_steal.run_autonomous()
             while self.isAutonomous() and self.isEnabled():
                 tinit = time.time()
                 self.sp.poll()
                 self.safeSleep(tinit, .04)
-            self.basic_auto.stop_autonomous()
-            self.one_bin_steal.stop_autonomous()
-            self.two_bin_steal.stop_autonomous()
+            
     else:
         def autonomous(self):
             pass
     
     def operatorControl(self):
         if auto_exists:
-            self.basic_auto.stop_autonomous()
-            self.one_bin_steal.stop_autonomous()
-            self.two_bin_steal.stop_autonomous()
+            pass
         while self.isOperatorControl() and self.isEnabled():
             tinit = time.time()
-            #self.teleop_controller.poll()
             self.sp.poll()
             self.hid_sp.poll()
             self.safeSleep(tinit, .04)
