19876570ba53bcc14eb41dd39963135257a76fa7
Alex Gao <alexgao12@gmail.com>
2016-12-07 17:28:31 -0800

    fixed gyro sp bugs

--- py/config.py
+++ py/config.py
@@ -7,7 +7,7 @@ from wpilib import Solenoid, Compressor, DriverStation, CANTalon
 
 from grt.sensors.attack_joystick import Attack3Joystick
 from grt.sensors.xbox_joystick import XboxJoystick
-from grt.sensors.gyro import Gyro
+#from grt.sensors.gyro import Gyro
 from grt.core import SensorPoller
 from grt.mechanism.drivetrain import DriveTrain
 from grt.mechanism.drivecontroller import ArcadeDriveController
@@ -23,31 +23,31 @@ from grt.mechanism.GMechs import *
 
 dt_right = CANTalon(1)
 dt_r2 = CANTalon(2)
-dt_r3 = CANTalon(3)
-dt_r4 = CANTalon(4)
+#dt_r3 = CANTalon(3)
+#dt_r4 = CANTalon(4)
 
-dt_left = CANTalon(7)
-dt_l2 = CANTalon(8)
-dt_l3 = CANTalon(9)
-dt_l4 = CANTalon(10)
+dt_left = CANTalon(3)
+dt_l2 = CANTalon(4)
+#dt_l3 = CANTalon(9)
+#dt_l4 = CANTalon(10)
 
 dt_r2.changeControlMode(CANTalon.ControlMode.Follower)
-dt_r3.changeControlMode(CANTalon.ControlMode.Follower)
-dt_r4.changeControlMode(CANTalon.ControlMode.Follower)
+#dt_r3.changeControlMode(CANTalon.ControlMode.Follower)
+#dt_r4.changeControlMode(CANTalon.ControlMode.Follower)
 dt_l2.changeControlMode(CANTalon.ControlMode.Follower)
-dt_l3.changeControlMode(CANTalon.ControlMode.Follower)
-dt_l4.changeControlMode(CANTalon.ControlMode.Follower)
-dt_r2.set(1)
-dt_r3.set(1)
-dt_r4.set(1)
-dt_l2.set(7)
-dt_l3.set(7)
-dt_l4.set(7)
+#dt_l3.changeControlMode(CANTalon.ControlMode.Follower)
+#dt_l4.changeControlMode(CANTalon.ControlMode.Follower)
+dt_r2.set(3)
+#dt_r3.set(1)
+#dt_r4.set(1)
+dt_l2.set(1)
+#dt_l3.set(3)
+#dt_l4.set(3)
 
 dt = DriveTrain(dt_left, dt_right, left_encoder=None, right_encoder=None)
 
-om_open = CANTalon(11)
-om_move = CANTalon(12)
+om_open = CANTalon(5)
+om_move = CANTalon(6)
 pu_open = Solenoid(0)
 pu_up = Solenoid(1)
 
@@ -55,7 +55,7 @@ om = Opener(om_open, om_move)
 pu = Clamp(pu_up, pu_open)
 
 #Skeleton sensor poller
-gyro = Gyro(1)
+#gyro = Gyro(1)
 # define sensor poller
 # sp = SensorPoller()
 
