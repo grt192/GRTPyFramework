30345d36b42c2f1e13516efe3f594c64695d8649
Chris Tian <pangeacake@outlook.com>
2017-10-18 14:00:54 -0700

    Readd Apple

--- py/config.py
+++ py/config.py
@@ -8,27 +8,35 @@ from ctre import CANTalon
 
 from grt.sensors.attack_joystick import Attack3Joystick
 from grt.sensors.xbox_joystick import XboxJoystick
-
+# from grt.sensors.gyro import Gyro
 from grt.core import SensorPoller
-#from grt.sensors.gyro import Gyro
+from grt.mechanism.drivetrain import DriveTrain
+from grt.mechanism.drivecontroller import ArcadeDriveController
+from grt.mechanism.motorset import Motorset
+from grt.sensors.ticker import Ticker
 from grt.sensors.encoder import Encoder
 from grt.sensors.talon import Talon
 from grt.mechanism.mechcontroller import MechController
 from grt.mechanism.spider import Spider
 from grt.mechanism.cookie import Cookie
 
+apple_motor = CANTalon(11)
+apple_p1 = Solenoid(1)
+apple_p2 = Solenoid(2)
+apple_mech = Apple(apple_motor, apple_p1, apple_p2)
 
-spider_actuator = Solenoid(1)
-
+spider_actuator = Solenoid(0)
 spider = Spider(spider_actuator)
 
-p3 = Solenoid(1)
-cookie = Cookie(p3)
+cookie_p1 = Solenoid(1)
+cookie = Cookie(cookie_p1)
 
 
 # Drive Controllers
 driver_stick = Attack3Joystick(0)
 xbox_controller = XboxJoystick(1)
+
+# ac = ArcadeDriveController(dt, driver_stick)
 hid_sp = SensorPoller((driver_stick, xbox_controller))  # human interface devices
 
 
--- py/grt/mechanism/mechcontroller.py
+++ py/grt/mechanism/mechcontroller.py
@@ -1,3 +1,5 @@
+import time
+
 class MechController:
 
     def __init__(self, spider, cookie, driver_joystick, xbox_controller): # mechanisms belong in arguments
@@ -10,6 +12,14 @@ class MechController:
         xbox_controller.add_listener(self._xbox_controller_listener)
 
     def _xbox_controller_listener(self, sensor, state_id, datum):
+        if state_id == 'a_button':
+            if datum:
+                self.apple_mech.close_curtains()
+                time.sleep(.5)
+                self.apple_mech.rotate()
+                time.sleep(.5)
+                self.apple_mech.open_curtains()
+                time.sleep(.5)
         if state_id == 'b_button':
             if datum:
                 self.spider.lower()
@@ -24,4 +34,10 @@ class MechController:
                 time.sleep(2)
 
     def _driver_joystick_listener(self, sensor, state_id, datum):
-        pass
+        if state_id == "switch1":
+            if datum:
+                print("switch1")
+                self.spider.lower()
+                time.sleep(5)
+                self.spider.raise_()
+                time.sleep(5)
