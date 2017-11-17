5cd4f5ef15233bc47a74245a5c0eac996752e76b
Chela <chela@Chelas-MacBook-Air-2.local>
2017-11-16 16:37:00 -0800

    driving works

--- py/config.py
+++ py/config.py
@@ -3,8 +3,7 @@ Config File for Robot
 """
 
 #@dhruv_rajan is editing config.py
-from wpilib import Solenoid, Compressor, DriverStation, DigitalInput
-from ctre import CANTalon
+from wpilib import Solenoid, CANTalon, Compressor, DriverStation, DigitalInput
 
 from grt.sensors.attack_joystick import Attack3Joystick
 from grt.sensors.xbox_joystick import XboxJoystick
@@ -17,37 +16,27 @@ from grt.sensors.ticker import Ticker
 from grt.sensors.encoder import Encoder
 from grt.sensors.talon import Talon
 from grt.mechanism.mechcontroller import MechController
-from grt.mechanism.swerve import Swerve
 
 
 #DT Talons and Objects
 
-turn_l2 = CANTalon(4) 
-turn_l2.changeControlMode(CANTalon.ControlMode.Position)
-turn_l2.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder)
-turn_l2.setPID(1.0, 0.0, 0.0)
-
-turn_r2 = CANTalon(1)
-turn_r2.changeControlMode(CANTalon.ControlMode.Position)
-turn_r2.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder)
-turn_r2.setPID(1.0, 0.0, 0.0)
-
-turn_left = CANTalon(5)
-turn_left.changeControlMode(CANTalon.ControlMode.Position)
-turn_left.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder)
-turn_left.setPID(1.0, 0.0, 0.0)
-
-turn_right = CANTalon(6)#5
-turn_right.changeControlMode(CANTalon.ControlMode.Position)
-turn_right.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder)
-turn_right.setPID(1.0, 0.0, 0.0)
+dt_right = CANTalon(11)#1
+dt_r2 = CANTalon(12)#2
+dt_r3 = CANTalon(13)#3
+dt_left = CANTalon(1)#11
+dt_l2 = CANTalon(2)#12
+dt_l3 = CANTalon(3)#13
+dt_shifter = Solenoid(0)
 
 
-dt_right = CANTalon(9)
-dt_left = CANTalon(7)
-
-dt_l2 = CANTalon(3)
-dt_r2 = CANTalon(10)
+dt_r2.changeControlMode(CANTalon.ControlMode.Follower)
+dt_r3.changeControlMode(CANTalon.ControlMode.Follower)
+dt_l2.changeControlMode(CANTalon.ControlMode.Follower)
+dt_l3.changeControlMode(CANTalon.ControlMode.Follower)
+dt_r2.set(dt_right.getDeviceID())
+dt_r3.set(dt_right.getDeviceID())
+dt_l2.set(dt_left.getDeviceID())
+dt_l3.set(dt_left.getDeviceID())
 
 
 # shooter1_m1 = CANTalon(11)
@@ -59,21 +48,19 @@ dt_r2 = CANTalon(10)
 #gyro = Gyro(1)
 # define sensor poller
 # sp = SensorPoller()
-
-swerve = Swerve(turn_right, turn_r2, turn_left, turn_l2, dt_right, dt_r2, dt_left, dt_l2)
-
+dt = DriveTrain(dt_left, dt_right, left_encoder=None, right_encoder=None)
 
 # Drive Controllers
 driver_stick = Attack3Joystick(0)
 xbox_controller = XboxJoystick(1)
 hid_sp = SensorPoller((driver_stick, xbox_controller))  # human interface devices
 
-
+ac = ArcadeDriveController(dt, driver_stick)
 
 # Mech Talons, objects, and controller
 
 # define MechController
-mc = MechController(driver_stick, xbox_controller, swerve)
+mc = MechController(driver_stick, xbox_controller)
 
 # define DriverStation
 ds = DriverStation.getInstance()
--- py/grt/mechanism/cat.py
+++
@@ -1,9 +0,0 @@
-class Cat:
-	def __init__(self, motor):
-		self.motor = motor
-		
-	def turn(self):
-		self.motor.set(0.5)
-		
-	def stop_turning(self):
-		self.motor.set(0)
